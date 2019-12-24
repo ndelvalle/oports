b40a06bca0e0f9afed30749bb33235d610682ef6
ndelvalle <nicolas.delvalle@gmail.com>
2019-12-24 01:57:30 -0300

    Add is open port and get open ports by range methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,38 @@ use async_std::net::{IpAddr, TcpStream};
 
 const MAX_PORT: u16 = 65535;
 
-pub async fn get_open_ports(ip: IpAddr) -> Vec<u16> {
-    let mut open = vec![];
+pub struct Oports {
+    address: IpAddr,
+}
 
-    for port in 0..MAX_PORT {
-        match TcpStream::connect((ip, port)).await {
-            Ok(_) => open.push(port),
-            Err(_) => {}
+impl Oports {
+    pub fn new(ip: IpAddr) -> Self {
+        Self { address: ip }
+    }
+
+    pub async fn is_port_open(&self, port: u16) -> bool {
+        match TcpStream::connect((self.address, port)).await {
+            Ok(_) => true,
+            Err(_) => false,
         }
     }
 
-    open
+    pub async fn open_ports_by_range(&self, from: u16, to: u16) -> Vec<u16> {
+        let mut open_ports = vec![];
+
+        for port in from..to {
+            println!(" port, {} ", port);
+            let is_open = self.is_port_open(port).await;
+            if is_open {
+                open_ports.push(port)
+            }
+        }
+        open_ports
+    }
+
+    pub async fn open_ports(&self) -> Vec<u16> {
+        self.open_ports_by_range(0, MAX_PORT).await
+    }
 }
 
 #[cfg(test)]
@@ -22,10 +43,22 @@ mod tests {
     use futures::executor::block_on;
 
     #[test]
+    fn it_should_return_open_ports_by_range() {
+        let ip_v4_addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let op = Oports::new(ip_v4_addr);
+
+        let future = op.open_ports_by_range(0, 10);
+        let result = block_on(future);
+
+        println!("Result {:?}", result);
+    }
+
+    #[test]
     fn it_should_return_open_ports() {
-        assert_eq!(2 + 2, 4);
-        let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-        let future = get_open_ports(ip);
+        let ip_v4_addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let op = Oports::new(ip_v4_addr);
+
+        let future = op.open_ports();
         let result = block_on(future);
 
         println!("Result {:?}", result);
