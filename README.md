53225c5e15e82b3a674e768e9c38de49f645d090
ndelvalle <nicolas.delvalle@gmail.com>
2019-12-24 17:58:13 -0300

    Add readme and husky tasks

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # oports
+
+![](https://github.com/ndelvalle/oports/workflows/Rust/badge.svg)
+
 Async library to retrieve open ports for a given IP address
+
+## Install
+
+Add `oports` as a dependency in the cargo.toml file if your project:
+
+```toml
+[dependencies]
+oports = "0.1"
+```
+
+If you have [cargo-edit](https://github.com/killercup/cargo-edit) utility tool
+installed, use:
+
+```bash
+$ cargo add oports
+```
+
+## Use
+
+```rust
+use Oports;
+use std::net::IpAddr;
+
+let ip_v4_addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+let op = Oports::new(ip_v4_addr);
+
+// Check if the given port is open or not
+let is_open_port = op.is_port_open(4040).await;
+
+// Retrieve a vec with open port for a given port range
+let open_ports_by_range = op.open_ports_by_range(0, 10).await;
+
+// Retrieve a vec with open port for all ports (0 - 65535)
+let open_ports = op.open_ports().await;
+```
--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,13 @@ impl Oports {
     }
 
     pub async fn is_port_open(&self, port: u16) -> bool {
-        match TcpStream::connect((self.address, port)).await {
-            Ok(_) => true,
-            Err(_) => false,
-        }
+        TcpStream::connect((self.address, port)).await.is_ok()
     }
 
     pub async fn open_ports_by_range(&self, from: u16, to: u16) -> Vec<u16> {
         let mut open_ports = vec![];
 
         for port in from..to {
-            println!(" port, {} ", port);
             let is_open = self.is_port_open(port).await;
             if is_open {
                 open_ports.push(port)
@@ -39,28 +35,39 @@ impl Oports {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use async_std::net::{IpAddr, Ipv4Addr};
     use futures::executor::block_on;
+    use std::net::{IpAddr, Ipv4Addr, TcpListener};
 
     #[test]
-    fn it_should_return_open_ports_by_range() {
+    fn it_should_determine_if_a_port_is_open() {
+        // TODO: It would be nice to shutdown the connection cleanly after the
+        //       assertion was made.
+        let _listener = TcpListener::bind("127.0.0.1:4040").unwrap();
+
         let ip_v4_addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
         let op = Oports::new(ip_v4_addr);
 
-        let future = op.open_ports_by_range(0, 10);
-        let result = block_on(future);
+        let is_port_8585_open = block_on(op.is_port_open(4040));
+        let is_port_2222_open = block_on(op.is_port_open(4041));
 
-        println!("Result {:?}", result);
+        assert_eq!(is_port_8585_open, true);
+        assert_eq!(is_port_2222_open, false);
     }
 
     #[test]
-    fn it_should_return_open_ports() {
+    fn it_should_return_open_ports_by_range() {
+        // TODO: It would be nice to shutdown the connection cleanly after the
+        //       assertion was made.
+        let _listener = TcpListener::bind("127.0.0.1:4045").unwrap();
+
         let ip_v4_addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
         let op = Oports::new(ip_v4_addr);
 
-        let future = op.open_ports();
-        let result = block_on(future);
+        let open_ports_a = block_on(op.open_ports_by_range(0, 10));
+        let open_ports_b = block_on(op.open_ports_by_range(4000, 4100));
 
-        println!("Result {:?}", result);
+        assert_eq!(open_ports_a.len(), 0);
+        assert_eq!(open_ports_b.len(), 1);
+        assert_eq!(open_ports_b[0], 4045);
     }
 }
